f04354c297f49e5a28f4f24dad7c29844ea219ed
RaliLy <121630818+RaliLy@users.noreply.github.com>
2024-11-04 13:41:46 +0100

    nav change

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ export default function Nav() {
   return (
     <nav>
       <NavLink to="/">Home</NavLink>
-      <NavLink to="/create">Create Post</NavLink>
+      <NavLink to="/create">Create Your Post</NavLink>
       <NavLink to="/recipes">Recipes</NavLink>
     </nav>
   );
